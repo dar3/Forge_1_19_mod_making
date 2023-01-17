7882f505226dbfa91690de6e214e94ca76f3b27b
Dariusz Szypka <darkoshypka@gmail.com>
2023-01-17 13:56:50 +0100

    Created a Custom Creative Tab with Zircon items in it.

--- src/main/java/net/darko/tutorialmod/item/ModItems.java
+++ src/main/java/net/darko/tutorialmod/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.darko.tutorialmod.item;
 
 import net.darko.tutorialmod.TutorialMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,9 +13,9 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
-                () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
